879d7c00e85995d47ba159ba8dee5592f4fa5b22
kkaan <kankean@gmail.com>
2021-06-03 12:27:10 +1000

    added aperture centroid detection

--- detectcircles.py
+++ detectcircles.py
@@ -11,8 +11,11 @@ import matplotlib.pyplot as plt
 from skimage import data, color
 from skimage.transform import hough_circle, hough_circle_peaks
 from skimage.feature import canny
+from skimage.measure import find_contours, regionprops, label
+from skimage.filters import threshold_otsu
 from skimage.draw import circle_perimeter
 from skimage.util import img_as_ubyte
+from scipy.ndimage.measurements import center_of_mass
 from PIL import Image
 
 
@@ -22,43 +25,101 @@ def crop_center(img,cropx,cropy):
     starty = y//2-(cropy//2)    
     return img[starty:starty+cropy,startx:startx+cropx]
 
+
+
 # Load picture and detect edges
-image = Image.open('P:/14 Projects/49_SRS Phantom/Ballz,Poder_6XFFF_210505_1627/MV/Ch0_1_668_173.24.tiff')
+image = Image.open('P:/14 Projects/49_SRS Phantom/Ballz,Poder_6XFFF_210505_1627/MV/Ch0_1_661_175.99.tiff')
 image = np.array(image)
 
 #"P:\14 Projects\49_SRS Phantom\Ballz,Poder_6XFFF_210505_1627\MV\Ch0_1_668_173.24.tiff"
 
-image = crop_center(image, 500, 650)
+image = crop_center(image, 900, 900)
+thresh = threshold_otsu(image)
+binary = image > thresh
+sel = np.zeros_like(image)
+sel[binary] = image[binary]
 
-edges = canny(image, sigma=2, low_threshold=0, high_threshold=50)
+def get_apeture_centroids(image):
+    thresh = threshold_otsu(image)
+    binary = image > thresh
+    
+    label_image = label(binary)
+    apertures = regionprops(label_image)
+    centroids = [a.centroid for a in apertures]
+    
+    # fig, axes = plt.subplots(ncols=3, figsize=(8, 2.5))
+    # ax = axes.ravel()
+    # ax[0] = plt.subplot(1, 3, 1)
+    # ax[1] = plt.subplot(1, 3, 2)
+    # ax[2] = plt.subplot(1, 3, 3, sharex=ax[0], sharey=ax[0])
+    
+    # ax[0].imshow(image, cmap=plt.cm.)
+    # ax[0].set_title('Original')
+    # ax[0].axis('off')
+    
+    # ax[1].hist(image.ravel(), bins=256)
+    # ax[1].set_title('Histogram')
+    # ax[1].axvline(thresh, color='r')
+    
+    # ax[2].imshow(binary, cmap=plt.cm.gray)
+    # ax[2].set_title('Thresholded')
+    # ax[2].axis('off')
+    
+    # plt.show()
+    
+    #Find contours at a constant value of 0.8
+    contours = find_contours(binary, 0.8)
+
+
+    # Display the image and plot all contours found
+    fig, ax = plt.subplots()
+    ax.imshow(label_image, cmap=plt.cm.Blues)
+
+    # for contour in contours:
+    #     ax.plot(contour[:, 1], contour[:, 0], linewidth=2)
+    
+    for centroid in centroids:
+        ax.plot(centroid[1], centroid[0], marker='o', linewidth=2, markersize=2)
+    
+    ax.axis('image')
+    ax.set_xticks([])
+    ax.set_yticks([])
+    plt.show()
+    
+    return contours, centroids
+
+contours, centroids = get_apeture_centroids(image)
+
+edges = canny(sel, sigma=2, low_threshold=10, high_threshold=50)
 
 
 
-fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(8, 3))
-ax.imshow(edges, cmap='gray')
-ax.set_title('lowT=0, highT=50')
+# fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(8, 3))
+# ax.imshow(edges, cmap='gray')
+# ax.set_title('lowT=0, highT=50')
 
 # Detect two radii
-hough_radii = np.arange(6, 15, 1)
+hough_radii = np.arange(4, 15)
 hough_res = hough_circle(edges, hough_radii)
 
-# Select the most prominent 3 circles
+# Select the most prominent 4 circles
 accums, cx, cy, radii = hough_circle_peaks(hough_res, hough_radii,
-                                           total_num_peaks=4)
+                                            total_num_peaks=4)
 
 # Draw them
-fig, ax = plt.subplots(ncols=1, nrows=1, figsize=(10, 4))
+fig, ax = plt.subplots()
 image = color.gray2rgb(image)
-for center_y, center_x, radius in zip(cy, cx, radii):
-    circy, circx = circle_perimeter(center_y, center_x, radius,
-                                    shape=image.shape)
-    image[circy, circx] = (220, 20, 20)
+# for center_y, center_x, radius in zip(cy, cx, radii):
+#     circy, circx = circle_perimeter(center_y, center_x, radius,
+#                                     shape=image.shape)
+#     image[circy, circx] = (500, 20, 20)
 
-ax.imshow(image, cmap="rainbow")
+for centroid in centroids:
+        ax.plot(centroid[1], centroid[0], color="darkred", marker='x', linewidth=3, markersize=5)
+
+for center_y, center_x in zip(cy, cx):
+        ax.plot(center_x, center_y, color="darkblue",marker='o', linewidth=3, markersize=2)
+
+ax.imshow(image)
 plt.show()
 
-def crop_center(img,cropx,cropy):
-    y,x = img.shape
-    startx = x//2-(cropx//2)
-    starty = y//2-(cropy//2)    
-    return img[starty:starty+cropy,startx:startx+cropx]
---
+++ gantryballcoordinates.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Ask for a folder of framegrabber images.
+Send each image to the detectcircles function to get back image coordinates
+Get an array of ball coordinates for all images.
+Plot coordinates against the gantry angle.
+
+Label and identify the balls by the Y position in the images. 
+
+When couch and gantry are changed we can 
+
+
+Created on Sat May  8 09:33:51 2021
+
+@author: 56153805
+"""
+
