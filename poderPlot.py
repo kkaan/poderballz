14b25e8bc59db5e2075d4175c75f1ecd316ad845
kkaan <kankean@gmail.com>
2021-08-05 11:48:16 +1000

    Update poderPlot.py

--- poderPlot.py
+++ poderPlot.py
@@ -13,10 +13,10 @@ import numpy as np
 # Creating dataset
 np.random.seed(10)
 
-data_1 = [0.0, 0.4, 0.5, 0.6]
-data_2 = [0.4, 0.6, 0.9, 0.5]
-data_3 = [0.7, 0.6, 0.4, 1.0]
-data_4 = [0.7, 1.2, 0.5, 1.5]
+data_4 = [0.80,	0.20, 0.00,	0.90, 1.20,	0.20, 0.10,	0.70, 0.40,	0.10, 0.80,	0.70]
+data_2 = [0.50, 0.20, 0.00, 0.70, 1.10, 0.10, 0.20,	0.10, 0.30,	0.20, 0.50,	0.30]
+data_3 = [0.20,	0.40, 0.20,	0.50, 1.20,	0.10, 0.20,	0.40, 0.60,	0.30, 0.70,	0.30]
+data_1 = [0.00,	0.20, 0.10,	0.20, 0.70,	0.20, 0.10,	0.30, 0.30,	0.70, 0.20,	0.00]
 
 # data_1 = np.random.normal(0.2, 0.1, 10)
 # data_2 = np.random.normal(0.32, 0.12, 10)
@@ -31,10 +31,12 @@ ax = fig.add_axes([0, 0, 1, 1])
  
 # Creating plot
 bp = ax.boxplot(data)
-ticks = plt.xticks([1,2,3,4],[0,30,60,100])
+ticks = plt.xticks([1,2,3,4],[30,50,60,100])
 plt.title(label="MTSI Positional Verification", fontsize=25)
-plt.xlabel("Distance from Isocentre (mm)", fontsize=25)
-plt.ylabel("Deviation from DRR (mm)", fontsize=25)
+plt.xlabel("Distance from Isocentre (mm)", fontsize=10)
+plt.ylabel("Deviation from DRR (mm)", fontsize=10)
 
 # show plot
 plt.show()
+
+
